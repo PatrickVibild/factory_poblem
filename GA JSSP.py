6ee55d4f83240abc8d5f6f6b27de46b5e0c08a91
patrickvibild <patrickvibild@gmail.com>
2022-11-19 22:37:12 +0900

    push working code.

--- GA JSSP.py
+++ GA JSSP.py
@@ -13,19 +13,45 @@ import matplotlib.pyplot as plt
 
 
 def run():
+    re_entry = "JSP_dataset_re_entry.xlsx"
+    entry = "JSP_dataset.xlsx"
+    using = re_entry
     ''' ================= initialization setting ======================'''
-    pt_tmp = pd.read_excel("JSP_dataset.xlsx", sheet_name="Processing Time", index_col=[0])
-    ms_tmp = pd.read_excel("JSP_dataset.xlsx", sheet_name="Machines Sequence", index_col=[0])
-    dependency_tmp = pd.read_excel("JSP_dataset.xlsx", sheet_name="Sequence Dependency", index_col=[0])
+    pt_tmp = pd.read_excel(using, sheet_name="Processing Time", index_col=[0])
+    ms_tmp = pd.read_excel(using, sheet_name="Machines Sequence", index_col=[0])
+    dependency_tmp = pd.read_excel(using, sheet_name="Sequence Dependency", index_col=[0])
+    machine_parallel = pd.read_excel(using, sheet_name="Machines", index_col=[0])
+    m_dep_tmp = pd.read_excel(using, sheet_name="Machine Dependency", index_col=[0])
 
     dfshape = pt_tmp.shape
-    num_mc = dfshape[1]  # number of machines
-    num_job = dfshape[0]  # number of jobs
-    num_gene = num_mc * num_job  # number of genes in a chromosome
+    num_tasks = dfshape[0]
+    num_jobs = ms_tmp.shape[1]
+    # number of jobs
 
-    processing_time = [list(map(int, pt_tmp.iloc[i])) for i in range(num_job)]
-    machine_sequence = [list(map(int, ms_tmp.iloc[i])) for i in range(num_job)]
-    dependency_time = [list(map(int, dependency_tmp.iloc[i])) for i in range(num_job)]
+    processing_time = [list(map(int, pt_tmp.iloc[i])) for i in range(num_tasks)]
+    machine_sequence = [list(map(int, ms_tmp.iloc[i])) for i in range(num_tasks)]
+    dependency_time = [list(map(int, dependency_tmp.iloc[i])) for i in range(num_tasks)]
+    machines = [list(map(int, machine_parallel.iloc[i])) for i in range(1)][0]
+    num_mc = max(max(machine_sequence))  # number of machines
+    num_gene = num_mc * num_jobs  # number of genes in a chromosome
+
+    # load dependencies. 1 is open 2 is close gate.
+    machine_dependency = [list(map(int, m_dep_tmp.iloc[i])) for i in range(num_tasks)]
+    # verify that dependencies is correct. At least one 1 in each sequence, and length equal to parallel machines.
+    for job in machine_dependency:
+        for j in range(num_tasks):
+            stringfy = str(job[j])
+            gates = list(stringfy)
+            if not gates.__contains__('1') or len(gates) != machines[j]:
+                raise ValueError('All jobs need a 1 as a gate and length equal to number of machines')
+            bool_gate = []
+            for gate in gates:
+                if gate == '1':
+                    bool_gate.append(True)
+                else:
+                    bool_gate.append(False)
+            job[j] = bool_gate
+
     # raw_input is used in python 2
     population_size = int(input('Please input the size of population: ') or 30)  # default value is 30
     crossover_rate = float(input('Please input the size of Crossover Rate: ') or 0.8)  # default value is 0.8
@@ -36,18 +62,33 @@ def run():
 
     start_time = time.time()
 
+    '''
+    Detect if its a Flex flow shop.
+    '''
+    is_flow = True
+    # compare that all jobs are equal in sequence to job1
+    for job in machine_sequence:
+        if job != machine_sequence[0]:
+            is_flow = False
+            break
     '''==================== main code ==============================='''
     '''----- generate initial population -----'''
     Tbest = 9e10
     best_list, best_obj = [], []
     population_list = []
     makespan_record = []
+
     for i in range(population_size):
-        nxm_random_num = list(np.random.permutation(num_gene))  # generate a random permutation of 0 to num_job*num_mc-1
+        # generate a random permutation of 0 to num_tasks*num_mc-1
+        nxm_random_num = list(np.random.permutation(num_gene))
+        nxm_random_num = list(np.float_(nxm_random_num))
+        # add floats to integers.
+        floats = np.random.uniform(low=0, high=1, size=(num_gene,))
+
         population_list.append(nxm_random_num)  # add to the population_list
         for j in range(num_gene):
             # convert to job number format, every job appears m times
-            population_list[i][j] = population_list[i][j] % num_job
+            population_list[i][j] = (population_list[i][j] % num_tasks) + floats[j]
 
     for n in range(num_iteration):
         Tbest_now = 9e10
@@ -73,11 +114,19 @@ def run():
                 offspring_list[S[2 * m]] = child_1[:]
                 offspring_list[S[2 * m + 1]] = child_2[:]
 
+        '''----------Strip decimals---------'''
+        offspring_decimals = np.empty((population_size,num_gene))
+        for i in range(population_size):
+            for j in range(num_gene):
+                offspring_decimals[i][j] = offspring_list[i][j] % 1
+                offspring_list[i][j] = int(offspring_list[i][j])
+        offspring_decimals = list(offspring_decimals)
         '''----------repairment-------------'''
         for m in range(population_size):
+
             job_count = {}
             larger, less = [], []  # 'larger' record jobs appear in the chromosome more than m times, and 'less' records less than m times.
-            for i in range(num_job):
+            for i in range(num_tasks):
                 if i in offspring_list[m]:
                     count = offspring_list[m].count(i)
                     pos = offspring_list[m].index(i)
@@ -85,36 +134,46 @@ def run():
                 else:
                     count = 0
                     job_count[i] = [count, 0]
-                if count > num_mc:
+                if count > num_jobs:
                     larger.append(i)
-                elif count < num_mc:
+                elif count < num_jobs:
                     less.append(i)
 
             for k in range(len(larger)):
                 chg_job = larger[k]
-                while job_count[chg_job][0] > num_mc:
+                while job_count[chg_job][0] > num_jobs:
                     for d in range(len(less)):
-                        if job_count[less[d]][0] < num_mc:
+                        if job_count[less[d]][0] < num_jobs:
+                            # here we repair sequence with new numbers. We also generate new floats for them.
+                            offspring_decimals[m][job_count[chg_job][1]] = np.random.uniform(low=0, high=1, size=(1,))
                             offspring_list[m][job_count[chg_job][1]] = less[d]
                             job_count[chg_job][1] = offspring_list[m].index(chg_job)
                             job_count[chg_job][0] = job_count[chg_job][0] - 1
                             job_count[less[d]][0] = job_count[less[d]][0] + 1
-                        if job_count[chg_job][0] == num_mc:
+                        if job_count[chg_job][0] == num_jobs:
                             break
 
-        '''--------mutatuon--------'''
+        '''--------mutation--------'''
         for m in range(len(offspring_list)):
             mutation_prob = np.random.rand()
             if mutation_rate >= mutation_prob:
-                m_chg = list(
-                    np.random.choice(num_gene, num_mutation_jobs, replace=False))  # chooses the position to mutation
-                t_value_last = offspring_list[m][m_chg[0]]  # save the value which is on the first mutation position
+                # chooses the position to mutation
+                m_chg = list(np.random.choice(num_gene, num_mutation_jobs, replace=False))
+                # save the value which is on the first mutation position
+                t_value_last = offspring_list[m][m_chg[0]]
+                t_value_last_decimal = offspring_decimals[m][m_chg[0]]
                 for i in range(num_mutation_jobs - 1):
-                    offspring_list[m][m_chg[i]] = offspring_list[m][m_chg[i + 1]]  # displacement
+                    # displacement
+                    offspring_list[m][m_chg[i]] = offspring_list[m][m_chg[i + 1]]
+                    offspring_decimals[m][m_chg[i]] = offspring_decimals[m][m_chg[i + 1]]
 
-                offspring_list[m][m_chg[
-                    num_mutation_jobs - 1]] = t_value_last  # move the value of the first mutation position to the last mutation position
+                # move the value of the first mutation position to the last mutation position
+                offspring_list[m][m_chg[num_mutation_jobs - 1]] = t_value_last
+                offspring_decimals[m][m_chg[num_mutation_jobs - 1]] = t_value_last_decimal
 
+        '''--------Concatenate decimals to integers--------------'''
+        offspring_list = np.float_(offspring_list)
+        offspring_list = list(np.add(offspring_list, offspring_decimals))
         '''--------fitness value(calculate makespan)-------------'''
         # parent and offspring chromosomes combination
         total_chromosome = copy.deepcopy(parent_list) + copy.deepcopy(offspring_list)
@@ -122,23 +181,58 @@ def run():
         total_fitness = 0
         for m in range(population_size * 2):
             # initialization of factory entry.
-            job_keys = [j for j in range(num_job)]
+            job_keys = [j for j in range(num_tasks)]
             key_count = {key: 0 for key in job_keys}
             job_count = {key: 0 for key in job_keys}
-            machine_keys = [j + 1 for j in range(num_mc)]
+            machine_keys = []
+            for i in range(len(machines)):
+                for j in range(machines[i]):
+                    machine_keys.append((i+1)*10 + j+1)
+
             machine_count = {key: 0 for key in machine_keys}
             machine_last_job_dict = {key: None for key in machine_keys}
-            for i in total_chromosome[m]:
+            for chromosome in total_chromosome[m]:
+                i = int(chromosome)
+                decimal = chromosome % 1
                 # time of assigning job 'i'. Excel Job = i+1
                 gen_t = int(processing_time[i][key_count[i]])
                 # Job assigned to machine gen_m.
+
+                # changing the machine depending on the decimal.
+                # the chromosome decimal decides what machine is being picked up.
                 gen_m = int(machine_sequence[i][key_count[i]])
+
+                suffix = 1
+
+                # loading machine options
+                dependency = machine_dependency[i][gen_m-1]
+                open_machines = dependency.count(True)
+                cut_out = 1 / open_machines
+                # here we obtain in which open machine we should allocate the job depending on its decimal.
+                while 1:
+                    if decimal < cut_out:
+                        break
+                    suffix += 1
+                    cut_out += cut_out
+
+                gated_suffix = 1
+                # here we transform the machine to the right one counting that it cna not be allocated to false machine
+                for d in dependency:
+                    if d is False:
+                        gated_suffix += 1
+                    else:
+                        suffix -= 1
+                        if suffix <= 0:
+                            break
+                        gated_suffix += 1
+                gen_m = gen_m*10 + gated_suffix
                 # adding sequence dependant times.
 
                 machine_job_shift = job_count[i] - machine_count[gen_m]
 
                 # The machine gen_m has been used previously and checking its time compared to current job.
-                if machine_last_job_dict[gen_m] is not None and machine_job_shift < dependency_time[machine_last_job_dict[gen_m]][i]:
+                if machine_last_job_dict[gen_m] is not None and machine_job_shift < \
+                        dependency_time[machine_last_job_dict[gen_m]][i]:
                     if machine_job_shift > 0:
                         changing_time = dependency_time[machine_last_job_dict[gen_m]][i] - machine_job_shift
                     else:
@@ -178,11 +272,11 @@ def run():
 
         for i in range(population_size):
             if selection_rand[i] <= qk[0]:
-                population_list[i] = copy.deepcopy(total_chromosome[0])
+                population_list[i] = list(copy.deepcopy(total_chromosome[0]))
             else:
                 for j in range(0, population_size * 2 - 1):
                     if selection_rand[i] > qk[j] and selection_rand[i] <= qk[j + 1]:
-                        population_list[i] = copy.deepcopy(total_chromosome[j + 1])
+                        population_list[i] = list(copy.deepcopy(total_chromosome[j + 1]))
                         break
         '''----------comparison----------'''
         for i in range(population_size * 2):
